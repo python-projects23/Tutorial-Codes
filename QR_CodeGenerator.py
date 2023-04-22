53ae537a1fdcec798fc9f297b0090df3aae54844
python-projects23 <python.projects2023@gmail.com>
2023-04-22 21:27:41 +0530

    qr code generator - latest code

--- QR_CodeGenerator.py
+++ QR_CodeGenerator.py
@@ -1,16 +1,31 @@
-# QR Code Generator
+# PYTHON QUICKIES #PROJECT 1
+# QR CODE GENERATOR
 
+# install qrcode using cmd command "pip install qrcode pillow"
 import qrcode
 
-data = "https://github.com/python-projects23/Tutorial-Codes/blob/master/printing_var.py"
+# define data
+data = "PYTHON QUICKIES QR CODE GENERATOR"
 
 # create QR code instance
 qr = qrcode.QRCode(version=1, box_size=10, border=5)
 
+# add data to the QR code instance
 qr.add_data(data)
 
+# make the QR code image
 qr.make(fit=True)
 
+# get the QR code image as PIL Image instance
 img = qr.make_image(fill_color="black", back_color="white")
 
-img.save("QR_code_Gen.png")
+# Name the file
+img.save("QR.png")
+
+
+
+
+
+
+
+
