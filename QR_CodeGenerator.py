9bd2a719ce9c9c41d2ee232c2d6b66e3e07e1043
python-projects23 <python.projects2023@gmail.com>
2023-04-22 18:26:41 +0530

    qr code generator

--- QR_CodeGenerator.py
+++ QR_CodeGenerator.py
@@ -1,1 +1,16 @@
 # QR Code Generator
+
+import qrcode
+
+data = "https://github.com/python-projects23/Tutorial-Codes/blob/master/printing_var.py"
+
+# create QR code instance
+qr = qrcode.QRCode(version=1, box_size=10, border=5)
+
+qr.add_data(data)
+
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save("QR_code_Gen.png")
